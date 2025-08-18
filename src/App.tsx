e0358169fa71b77cc72f1b6bdc02a8e99e08d206
[dyad] <tamadeu@hotmail.com>
2025-08-18 18:20:55 -0300

    [dyad] Create Budgets, Reports, and Settings pages - wrote 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 import Dashboard from "./pages/Dashboard";
 import AccountsPage from "./pages/Accounts";
 import AccountDetailPage from "./pages/AccountDetail";
 import TransactionsPage from "./pages/Transactions";
 import SearchResultsPage from "./pages/SearchResults";
+import BudgetsPage from "./pages/Budgets";
+import ReportsPage from "./pages/Reports";
+import SettingsPage from "./pages/Settings";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/accounts" element={<AccountsPage />} />
-        <Route path="/accounts/:accountId" element={<AccountDetailPage />} />
-        <Route path="/transactions" element={<TransactionsPage />} />
-        <Route path="/search" element={<SearchResultsPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/accounts" element={<AccountsPage />} />
+          <Route path="/accounts/:accountId" element={<AccountDetailPage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/search" element={<SearchResultsPage />} />
+          <Route path="/budgets" element={<BudgetsPage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </ThemeProvider>
   );
 }
 
