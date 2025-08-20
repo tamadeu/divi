fb20bbe50d198613e6aa826d7caef3ac68c1b4f2
[dyad] <tamadeu@hotmail.com>
2025-08-20 12:46:09 -0300

    Updated src/components/transactions/EditTransactionModal.tsx

--- src/components/transactions/EditTransactionModal.tsx
+++ src/components/transactions/EditTransactionModal.tsx
@@ -142,7 +142,6 @@ const EditTransactionModal = ({ isOpen, onClose, onTransactionUpdated, transacti
     const { data: categoriesData } = await supabase
       .from("categories")
       .select("*")
-      .eq("user_id", user.id)
       .order("name", { ascending: true });
     setCategories(categoriesData || []);
 
